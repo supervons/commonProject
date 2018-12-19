7f2db0087e1105ac9c865faa605e85ab11eb51f8
supervons <supervons@sina.com>
2018-12-19 23:27:46 +0800

    增加Native-Base UI组件。 add NativeBase Ui components

---
+++ commonPage/NativeBase/nativeBase.js
@@ -0,0 +1,22 @@
+/**
+ * Created by supervons on 2018/12/19.
+ */
+import React, { Component } from "react";
+import { Container, Header, Content, Accordion } from "native-base";
+const dataArray = [
+    { title: "First Element", content: "Lorem ipsum dolor sit amet" },
+    { title: "Second Element", content: "Lorem ipsum dolor sit amet" },
+    { title: "Third Element", content: "Lorem ipsum dolor sit amet" }
+];
+export default class AccordionIconExample extends Component {
+    render() {
+        return (
+            <Container>
+                <Header />
+                <Content padder>
+                    <Accordion dataArray={dataArray} icon="add" expandedIcon="remove" />
+                </Content>
+            </Container>
+        );
+    }
+}
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
+import nativeBase from './commonPage/NativeBase/nativeBase';
 
-AppRegistry.registerComponent('commonProject', () => App);
+AppRegistry.registerComponent('commonProject', () => nativeBase);
