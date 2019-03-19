5f642e00438a189290feca01430ce91a46204cab
supervons <supervons@sina.com>
2019-03-19 22:55:35 +0800

    1，新闻列表上拉加载，下拉刷新。
    1. The news list is pulled up and pulled down to refresh.

--- commonPage/News/news.js
+++ commonPage/News/news.js
@@ -4,34 +4,37 @@
  */
 import React, {Component} from 'react';
 import NewsItem from './newsItem';
-import { FlatList, RefreshControl, Image} from 'react-native';
-import NewsAction from '../Components/newsAction';
-import Spinner from '../Spinner/spinner';
 import {
-    Container,
-    Header,
-    Content,
-    Card,
-    CardItem,
-    Thumbnail,
+    FlatList,
     Text,
-    Button,
-    Icon,
-    Left,
-    Body,
-    Right,
-    Footer,
-    FooterTab,
-} from 'native-base';
+    RefreshControl,
+    InteractionManager,
+    StyleSheet,
+    Image,
+    View,
+    ActivityIndicator
+} from 'react-native';
+import NewsAction from '../Components/newsAction';
+
 export default class News extends Component {
 
     constructor(props) {
         super(props);
         this.returnRender = this.returnRender.bind(this);
+        this.fetchData = this.fetchData.bind(this);
+        this.handleRefresh = this.handleRefresh.bind(this);
+        this.renderSeparator = this.renderSeparator.bind(this);
+        this.keyId = 0;
         this.state = ({
-            newsList: [],
-            newsSpinner: true,
-            refreshing:true,
+            // 当前页码
+            pageNo: 0,
+            // 步长
+            itemNo: 5,
+            isLoading: true,
+            dataArray: [],
+            showFoot: 0, // 控制foot， 0：隐藏footer  1：已加载完成,没有更多数据   2 ：显示加载中
+            isRefreshing: false,//下拉控制
+            totalPageCount: 0,
         });
     }
 
@@ -43,25 +46,64 @@ export default class News extends Component {
         },
     }
 
-    componentDidMount(){
+    componentDidMount() {
+        InteractionManager.runAfterInteractions(() => {
+            this.fetchData();
+        });
+    }
+
+    fetchData() {
+        let pageNo = this.state.pageNo;
+        let itemNo = this.state.itemNo;
+        /**
+         * 请求接口
+         * pageNo 当前页面
+         * itemNo 步长
+         */
         NewsAction.getNewsList({
+            pageNo: pageNo,
+            itemNo: itemNo,
         }, (response) => {
-            console.log(JSON.stringify(response))
-            this.setState({
-                newsList: response,
-                newsSpinner: false,
-                refreshing:false,
+            let data = response.data;
+            let dataBlob = [];
+            let i = this.keyId;
+            data.map(function (item) {
+                dataBlob.push({
+                    key: i + '',
+                    ...item,
+                })
+                i++;
             });
+            let foot = 0;
+            this.keyId = i;
+            this.setState({
+                dataArray: this.state.dataArray.concat(dataBlob),
+                newsSpinner: false,
+                isRefreshing: false,
+                showFoot: foot,
+                totalPageCount: response.auxiliaryData.countNum,
+            });
+            data = null;
+            dataBlob = null;
         }, (error) => {
             this.setState({
                 newsSpinner: false,
-                refreshing:false,
+                refreshing: false,
             });
         });
     }
 
-    returnRender(item){
-        return(
+    handleRefresh = () => {
+        this.setState({
+            pageNo: 0,
+            isRefreshing: true,//tag,下拉刷新中，加载完全，就设置成flase
+            dataArray: []
+        });
+        this.fetchData()
+    }
+
+    returnRender(item) {
+        return (
             <NewsItem
                 newsTitle={item.newsTitle}
                 newsIntroduction={item.newsIntroduction}
@@ -71,61 +113,123 @@ export default class News extends Component {
         );
     }
 
-    _onRefresh(){
-        this.setState({
-            refreshing:true,
-        });
+    renderSeparator = () => {
+        return (
+            <View
+                style={{
+                    height: 1,
+                    backgroundColor: "#CED0CE",
+                }}
+            />
+        );
+    };
+
+    _onEndReached() {
+        let pageNo = this.state.pageNo;
+        let itemNO = this.state.itemNo;
+        let totalPage = Math.ceil(this.state.totalPageCount / itemNO);
+        //如果是正在加载中或没有更多数据了，则返回
+        if (this.state.showFoot != 0) {
+            return;
+        }
+        //如果当前页大于或等于总页数，那就是到最后一页了，返回
+        if ((pageNo != 0) && (pageNo >= totalPage)) {
+            this.setState({showFoot: 1});
+            return;
+        } else {
+            pageNo++;
+        }
+        //底部显示正在加载更多数据
+        this.setState({showFoot: 2, pageNo: pageNo},
+            this.fetchData);
+    }
+
+    _renderFooter() {
+        if (this.state.showFoot === 1) {
+            return (
+                <View style={{height: 30, alignItems: 'center', justifyContent: 'flex-start',}}>
+                    <Text style={{color: '#999999', fontSize: 14, marginTop: 5, marginBottom: 5,}}>
+                        - 我也是有底线的 -
+                    </Text>
+                </View>
+            );
+        } else if (this.state.showFoot === 2) {
+            return (
+                <View style={styles.footer}>
+                    <ActivityIndicator />
+                    <Text>正在加载更多数据...</Text>
+                </View>
+            );
+        } else if (this.state.showFoot === 0) {
+            return (
+                <View style={styles.footer}>
+                    <Text></Text>
+                </View>
+            );
+        }
+    }
+
+    _separator() {
+        return <View style={{height: 1, backgroundColor: '#999999'}}/>;
+    }
+
+    renderData() {
+        return (
+            <FlatList
+                data={this.state.dataArray}
+                renderItem={({item}) => this.returnRender(item)}
+                ListFooterComponent={this._renderFooter.bind(this)}
+                onEndReached={this._onEndReached.bind(this)}
+                onEndReachedThreshold={0.01}
+                ItemSeparatorComponent={this._separator}
+                refreshControl={
+                    <RefreshControl
+                        refreshing={this.state.isRefreshing}
+                        onRefresh={this.handleRefresh.bind(this)}//因为涉及到this.state
+                        title="努力加载中..."
+                        colors={['#ff0000', '#00ff00', '#0000ff', '#3ad564']}
+                        progressBackgroundColor="#ffffff"
+                    />
+                }
+            />
+
+        );
     }
 
     render() {
-        return (
-            <Container>
-                <Content>
-                    <Spinner
-                        showSpinner={this.state.newsSpinner}
-                        spinkerSize={50}
-                        spinkerType='Wave'
-                        spinkerColor='#3B77FF'/>
-                    <FlatList
-                        refreshing={this.state.refreshing}
-                        onRefresh={() => this._onRefresh()}
-                        data={this.state.newsList
-                            /*[
-                            {
-                                key:'1',
-                                newsItem: '阿丽塔上映',
-                                newsIntroduction: '《阿丽塔：战斗天使》是由二十世纪福克斯电影公司...',
-                                likeCount: '58',
-                                commentsCount: '9',
-                                timeStamp: '2015/03/12'
-                            },
-                            {   key:'2',
-                                newsItem: '阿丽塔上映',
-                                newsIntroduction: '《阿丽塔：战斗天使》是由二十世纪福克斯电影公司...',
-                                likeCount: '58',
-                                commentsCount: '9',
-                                timeStamp: '2015/03/12'
-                            },
-                            {   key:'3',
-                                newsItem: '阿丽塔上映',
-                                newsIntroduction: '《阿丽塔：战斗天使》是由二十世纪福克斯电影公司...',
-                                likeCount: '58',
-                                commentsCount: '9',
-                                timeStamp: '2015/03/12'
-                            },
-                            {   key:'4',
-                                newsItem: '阿丽塔上映',
-                                newsIntroduction: '《阿丽塔：战斗天使》是由二十世纪福克斯电影公司...',
-                                likeCount: '58',
-                                commentsCount: '9',
-                                timeStamp: '2015/03/12'
-                            },
-                        ]*/
-                        }
-                        renderItem={({item}) => this.returnRender(item)}
-                    />
-                </Content>
-            </Container>
-        );
+        //加载数据
+        return this.renderData();
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 10,
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    title: {
+        marginTop: 8,
+        marginLeft: 8,
+        marginRight: 8,
+        fontSize: 15,
+        color: '#ffa700',
+    },
+    footer: {
+        flexDirection: 'row',
+        height: 24,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 10,
+    },
+    content: {
+        marginBottom: 8,
+        marginLeft: 8,
+        marginRight: 8,
+        fontSize: 14,
+        color: 'black',
+    }
+});
